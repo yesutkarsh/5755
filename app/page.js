85dc93d292c004ead523fbdaf03d78568c67e8a8
yesutkarsh <contactutkarshverma@gmail.com>
2025-01-17 21:25:24 +0530

    Project Inception, Next js and Kinde Auth Setup

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+
+
+export default function Home() {
+  return (
+    <>
+        
+    </>
+  );
+}
